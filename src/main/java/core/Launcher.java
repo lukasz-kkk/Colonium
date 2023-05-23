0fce46772b2fd70897b3f9ad0d3a6084780350f9
lukasz-kkk <lukaluzny17@gmail.com>
2023-05-23 18:46:52 +0200

    rendering of map and provinces

--- src/main/java/core/Launcher.java
+++ src/main/java/core/Launcher.java
@@ -5,14 +5,15 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.badlogic.gdx.graphics.Color;
 
 public class Launcher {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setBackBufferConfig(8, 8, 8, 8, 16, 0, 16);
         config.setIdleFPS(60);
         config.useVsync(true);
         config.setTitle("Colonium");
 
-        //config.setWindowedMode(1920, 1080);
-    config.setMaximized(true);
+        config.setMaximized(true);
+
         config.setInitialBackgroundColor(new Color(0.5f, 0.5f, 0.6f, 0));
 
         new Lwjgl3Application(new Boot(), config);
