802fcfee507395ad9ba4fcea69e6b8cca86d6010
MaciejKrakowiak <240719@edu.p.lodz.pl>
2023-06-20 23:31:03 +0200

    fixed bug with endgame

--- src/main/java/UI/ArmyStrip.java
+++ src/main/java/UI/ArmyStrip.java
@@ -21,7 +21,7 @@ public class ArmyStrip {
     private final SpriteBatch batch;
     public static String winner;
 
-    public ArmyStrip(List<String> players){
+    public ArmyStrip(List<String> players) {
         this.shapeRenderer = new ShapeRenderer();
         this.batch = new SpriteBatch();
         this.players = players;
@@ -29,72 +29,72 @@ public class ArmyStrip {
     }
 
 
-    public void render(){
-        if(players == null) return;
+    public void render() {
+        if (players == null) return;
         playerRender();
     }
 
-    private void playerRender(){
+    private void playerRender() {
 
         int lastWidth = 490;
         int renderArmy = 0;
         int totalArmy = 0;
-        int losePlayerCounter = 0;
+        int losePlayerCounter = 1;
         float w = 0;
-        int width =0;
+        int width = 0;
 
-        for(int i = 0; i < Map.numberOfProvinces; i++)
-        {
+        for (int i = 0; i < Map.numberOfProvinces; i++) {
             totalArmy += Map.provinces[i].getValue();
 
         }
 
-        if(!UnitHandler.unitList.isEmpty())
-        {
+        if (!UnitHandler.unitList.isEmpty()) {
             totalArmy += UnitHandler.unitList.size();
         }
 
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(Color.BLACK);
-        shapeRenderer.rect(lastWidth-5,920-5, 1010, 60);
+        shapeRenderer.rect(lastWidth - 5, 920 - 5, 1010, 60);
         shapeRenderer.end();
 
-        for (int i=0;i<players.size();i++) {
+        for (int i = 0; i < players.size(); i++) {
             for (int j = 0; j < Map.numberOfProvinces; j++) {
-                if(Map.provinces[j].owner == null) return;
+                if (Map.provinces[j].owner == null) return;
                 if (Map.provinces[j].owner.equals(players.get(i))) {
-                    renderArmy +=Map.provinces[j].getValue();
+                    renderArmy += Map.provinces[j].getValue();
                 }
             }
-            if(!UnitHandler.unitList.isEmpty())
-            {
-                for(int j =0;j<UnitHandler.unitList.size();j++)
-                {
-                    if(UnitHandler.unitList.get(j).getOwner().equals(players.get(i))) renderArmy++;
+            if (!UnitHandler.unitList.isEmpty()) {
+                for (int j = 0; j < UnitHandler.unitList.size(); j++) {
+                    if (UnitHandler.unitList.get(j).getOwner().equals(players.get(i))) renderArmy++;
                 }
             }
-            if(renderArmy == 0 && players.size()!=i) losePlayerCounter++;
-            w = (1000*renderArmy)/totalArmy;
+            int np=0;
+            if (renderArmy == 0 && !players.get(i).equals("unowned")) {
+                for (int j = 0; j < Map.numberOfProvinces; j++) {
+                    if (Map.provinces[j].owner.equals(players.get(i))) np++;
+                }
+                if(np==0) losePlayerCounter++;
+            }
+            w = (1000 * renderArmy) / totalArmy;
             width = Math.round(w);
-            renderArmy=0;
+            renderArmy = 0;
             shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
             setColor(i);
-            shapeRenderer.rect(lastWidth,920, width, 50);
+            shapeRenderer.rect(lastWidth, 920, width, 50);
             lastWidth += width;
             shapeRenderer.end();
         }
-        if(losePlayerCounter==players.size()-2)
-        {
-            for (int i=0;i<players.size()-1;i++) {
-                renderArmy=0;
+
+        if (losePlayerCounter == players.size() - 1) {
+            for (int i = 0; i < players.size() - 1; i++) {
+                renderArmy = 0;
                 for (int j = 0; j < Map.numberOfProvinces; j++) {
-                    if (Map.provinces[j].owner == null) return;
                     if (Map.provinces[j].owner.equals(players.get(i))) {
                         renderArmy += Map.provinces[j].getValue();
                     }
                 }
-                if(renderArmy>0)
-                {
+                if (renderArmy > 0) {
                     winner = players.get(i);
                     Boot.INSTANCE.setScreen(Boot.endScreen);
                 }
@@ -102,9 +102,10 @@ public class ArmyStrip {
         }
     }
 
-    private void setColor(int playerID){
-        if(playerID == players.size()-1) shapeRenderer.setColor(Definitions.colors[0]);
-        else shapeRenderer.setColor(Definitions.colors[playerID+1]);
+    private void setColor(int playerID) {
+        if (playerID == players.size() - 1) shapeRenderer.setColor(Definitions.colors[0]);
+        else shapeRenderer.setColor(Definitions.colors[playerID + 1]);
     }
 
 }
+
